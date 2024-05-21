e478746d18513a1344d1c28d3ff8307dcfd56adb
davidsemakula <hello@davidsemakula.com>
2024-05-21 18:37:56 +0300

    enable test harness generation

--- src/driver.rs
+++ src/driver.rs
@@ -33,8 +33,8 @@ fn main() {
     // Generates tractable entry points for FRAME pallet.
     let mut entry_point_args = cli_args.clone();
     entry_point_args.extend([
-        // Enables compilation of unit tests.
-        "--cfg=test".to_owned(),
+        // Enables compilation of unit tests and test harness generation.
+        "--test".to_owned(),
         // Enables dumping MIR for all functions.
         "-Zalways-encode-mir".to_owned(),
     ]);
@@ -62,8 +62,8 @@ fn main() {
     // Analyzes FRAME pallet with MIRAI.
     let mut verifier_args = cli_args.clone();
     verifier_args.extend([
-        // Enables compilation of unit tests.
-        "--cfg=test".to_owned(),
+        // Enables compilation of unit tests and test harness generation.
+        "--test".to_owned(),
         // Enables compilation of MIRAI only code.
         "--cfg=mirai".to_owned(),
         // Enables dumping MIR for all functions.
